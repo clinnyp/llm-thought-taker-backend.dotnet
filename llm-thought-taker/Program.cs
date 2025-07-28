d68278747ed1b6f32f7dc0d47cfc2cf7fd29d526
clinnyp <cphilathong1@gmail.com>
2025-07-28 20:29:45 +1200

    app entrypoint

---
+++ llm-thought-taker/Program.cs
@@ -0,0 +1,34 @@
+using System.Text.Json;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using llm_thought_taker.Data;
+using Microsoft.Extensions.Hosting;
+using GenerativeAI;
+
+
+var builder = FunctionsApplication.CreateBuilder(args);
+builder.Services
+    .AddApplicationInsightsTelemetryWorkerService()
+    .ConfigureFunctionsApplicationInsights();
+
+builder.Services.Configure<JsonSerializerOptions>(options =>
+{
+    options.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
+});
+
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+var geminiApiKey = Environment.GetEnvironmentVariable("GEMINI_API_KEY");
+var googleAI = new GoogleAi(geminiApiKey);
+var model = googleAI.CreateGenerativeModel("models/gemini-2.0-flash-lite");
+
+// builder.Services.AddSingleton(googleAI);
+builder.Services.AddSingleton(model);
+
+
+
+builder.Build().Run();
